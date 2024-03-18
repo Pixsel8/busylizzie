edbe00bf456dc766ba246ea5152cc23f8efbfc03
ellmer <elliot@pixsel8.com>
2024-03-18 21:54:33 +0000

    Started working on the home page

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Button = ({ children, link }) => {
-  return (
-    <Link
-      to={link}
-      href="#"
-      className="rounded-3xl bg-gray-900 px-3.5 py-2.5 text-white hover:bg-pink-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-700"
-    >
-      {children}
-    </Link>
-  )
+const Button = ({ children, link, style }) => {
+  if (style === "light") {
+    return (
+      <Link
+        to={link}
+        href="#"
+        className="text-center w-3/5 md:w-48 rounded-3xl bg-pink-700 px-3.5 py-2.5 text-white hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-700"
+      >
+        {children}
+      </Link>
+    )
+  } else {
+    return (
+      <Link
+        to={link}
+        href="#"
+        className="text-center w-3/5 md:w-48 rounded-3xl bg-gray-900 px-3.5 py-2.5 text-white hover:bg-pink-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-700"
+      >
+        {children}
+      </Link>
+    )
+  }
 }
 
 export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,64 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/hero"
+import Button from "../components/button"
+import Testimonial from "../components/testimonial"
+import Contact from "../components/contact"
+
+const heroData = {
+  title: "Your welfare is our concern",
+  description:
+    "Busy Lizzie provides professional home care services tailored to your needs.",
+}
 
 const Index = () => {
+  const { title, description } = heroData
+  const { allFile } = useStaticQuery(graphql`
+    query {
+      allFile(filter: { name: { eq: "home-hero" } }) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  `)
+
+  // Assuming there's only one matching file, adjust as necessary for your data structure
+  const heroImageData = allFile.nodes[0].childImageSharp.gatsbyImageData
+
   return (
     <Layout>
-      <Hero />
       <Seo />
+      <Hero title={title} heroImage={heroImageData} description={description} />
+      <div className="bg-white my-20 lg:my-32 xl:mt-0 lg:px-8">
+        <div className="max-w-4xl mx-auto px-10 text-center">
+          <p>
+            Busy Lizzie Homecare is a small company that was started in 2018 by
+            Chloe Bean in Timsbury. The main areas covered are within an eight
+            mile radius of this village. Busy Lizzie have established a very
+            good reputation in this area and any prospective clients would be
+            more than welcome to ‘test local opinion’. The company prides itself
+            on it’s motto ‘Your welfare is our Concern’. Being small we can be
+            very flexible with respect to the services we provide. As we are
+            very keen to treat every client as an individual each package of
+            services will essentially be bespoke. Sensitivity to a clients
+            current and possibly changing requirements is central to the
+            services we provide.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-4 my-8 md:flex-row md:space-x-3 md:space-y-0 ">
+          <Button link="/about" style="light">
+            about us
+          </Button>
+          <Button link="/contact">contact us today</Button>
+        </div>
+      </div>
+      <Testimonial />
+      <Contact />
     </Layout>
   )
 }
