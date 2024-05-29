6194c8f099777f9135a012030f9d50bd8ef2757f
ellmer <elliot@pixsel8.com>
2024-05-29 15:19:19 +0100

    changeemail on contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -37,7 +37,7 @@ const contact = () => {
                   alt="message icon"
                   className="inline-block mx-4"
                 />
-                <h3 className="inline">info@busylizziecare.co.uk</h3>
+                <h3 className="inline">info@busylizziehomecare.co.uk</h3>
               </div>
               <div className="md:flex md:items-center">
                 <img
